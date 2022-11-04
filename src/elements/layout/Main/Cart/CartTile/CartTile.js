785326875425383f7784c5c31105786cf13d953e
peopleAlreadyKnowWhoIAm <nz54ds@gmail.com>
2022-11-04 13:17:51 +0200

    Micro impovement

--- src/elements/layout/Main/Cart/CartTile/CartTile.js
+++ src/elements/layout/Main/Cart/CartTile/CartTile.js
@@ -3,6 +3,7 @@ import AmountInput from "elements/components/AmountInput/AmountInput";
 import { useEffect, useState } from "react";
 import { CloseButton, Col, Container, Row } from "react-bootstrap";
 import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 export default function CartTile({ data }) {
     const [amount, setAmount] = useState(data.amount);
@@ -11,10 +12,12 @@ export default function CartTile({ data }) {
 
     const onRemove = () => dispatch(removeProduct({id:data.id}));
 
+    const navigate = useNavigate();
+
     useEffect(() => {dispatch(updateProduct({ id: data.id, amount: amount }))}, [amount, dispatch])
     return (
         <Row className="border border-primary rounded p-2 flex-basis-0 justify-content-between my-3">
-            <Col xs={7}>
+            <Col xs={7} onClick={()=> navigate(`/catalog/${data.id}`)} style={{"cursor": "pointer"}}>
                 <img alt={`Product: ${data.product.name}`} src={data.product.imageUrls[0]} style={{ width: "6rem", height: "6rem", objectFit: "cover", boxSizing: "content-box" }} />
                 <span className="mx-2 fw-semibold fs-4">
                     {data.product.name}
